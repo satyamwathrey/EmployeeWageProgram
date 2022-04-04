1e427af09dd435cadb47691486ac2013c5421584
satyamwathrey <swathrey1@gmail.com>
2022-04-04 16:24:48 +0530

    Employee Attendance

---
+++ EmpAttendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class EmpAttendance
+    {
+        const int EMP_PRESENT = 1;
+
+        public static void GetEmpAttendance()
+        {
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if (randomInput == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+    }
+}
