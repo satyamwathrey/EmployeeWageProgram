b3e619ccfa8fb181e346c7a7e6b4771699dde8ff
satyamwathrey <swathrey1@gmail.com>
2022-04-08 22:22:40 +0530

    Ability to save the Total Wage for each Company

--- Program.cs
+++ Program.cs
@@ -6,17 +6,31 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
-            EmpAttendance.GetEmpAttendance();
-            EmployeeWage.GetEmpWage();
-            PartTimeEmployee.PartTimeWage();
-            SwitchCase.DailyEmpWage();
-            MonthWages.MonthlyEmpWage();
-            WageTillCondition.GetWageTillCondition();
-            RefactorWithClassMethod.RefactorGetMonthlyEmpWage();
-            EmpWageForMultipleCompanies.ComputeEmpWage("Google", 20, 2, 10);
-            EmpWageForMultipleCompanies.ComputeEmpWage("Microsoft", 10, 4, 20);
-            EmpWageForMultipleCompanies.ComputeEmpWage("Deloitte", 15, 3, 15);
+            //Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
+            //EmpAttendance.GetEmpAttendance();
+            //EmployeeWage.GetEmpWage();
+            //PartTimeEmployee.PartTimeWage();
+            //SwitchCase.DailyEmpWage();
+            //MonthWages.MonthlyEmpWage();
+            //WageTillCondition.GetWageTillCondition();
+            //RefactorWithClassMethod.RefactorGetMonthlyEmpWage();
+            //EmpWageForMultipleCompanies.ComputeEmpWage("Google", 20, 2, 10);
+            //EmpWageForMultipleCompanies.ComputeEmpWage("Microsoft", 10, 4, 20);
+            //EmpWageForMultipleCompanies.ComputeEmpWage("Deloitte", 15, 3, 15);
+
+
+            SaveTotalWageByCompany google = new SaveTotalWageByCompany("Google", 10, 4, 20);
+            google.TotalWageForEachCompany();
+            Console.WriteLine(google.ToString());
+
+            SaveTotalWageByCompany microsoft = new SaveTotalWageByCompany("Microsoft", 20, 5, 20);
+            microsoft.TotalWageForEachCompany();
+            Console.WriteLine(microsoft.ToString());
+
+            SaveTotalWageByCompany deloitte = new SaveTotalWageByCompany("Deloitte", 30, 3, 30);
+            deloitte.TotalWageForEachCompany();
+            Console.WriteLine(deloitte.ToString());
+            
             Console.ReadLine();
         }
     }
---
+++ SaveTotalWageByCompany.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class SaveTotalWageByCompany
+    {
+        public const int EMP_FULLTIME = 1, EMP_PARTTIME = 2;
+        static Random random = new Random();
+
+        private string company;
+        private int emp_Wage_Per_Hr;
+        private int working_Days_Per_Month;
+        private int max_Hrs_Per_Month;
+        private int total_Emp_Wage;
+
+        public SaveTotalWageByCompany(string company, int emp_Wage_Per_Hr, int working_Days_Per_Month, int max_Hrs_Per_Month)
+        {
+            this.company = company;
+            this.emp_Wage_Per_Hr = emp_Wage_Per_Hr;
+            this.working_Days_Per_Month = working_Days_Per_Month;
+            this.max_Hrs_Per_Month = max_Hrs_Per_Month;
+        }
+
+        public void TotalWageForEachCompany()
+        {
+            int empHrs = 0, total_Emp_Hrs = 0, totalWorkingDays = 1, daily_Emp_Wage = 0;
+
+            while (totalWorkingDays <= working_Days_Per_Month && total_Emp_Hrs <= max_Hrs_Per_Month)
+            {
+                int randomInput = random.Next(0, 3);
+
+                switch (randomInput)
+                {
+                    case EMP_FULLTIME:
+                        empHrs = 8;
+                        break;
+                    case EMP_PARTTIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                daily_Emp_Wage = empHrs * emp_Wage_Per_Hr;
+                Console.WriteLine("Employee wage for day {0} is: {1} for {2} Hrs", totalWorkingDays, daily_Emp_Wage, empHrs);
+                total_Emp_Wage += daily_Emp_Wage;
+                total_Emp_Hrs += empHrs;
+                totalWorkingDays++;
+            }
+            Console.WriteLine("Total Days: {0}, Total working hours: {1}", (totalWorkingDays - 1), total_Emp_Hrs);
+            Console.WriteLine("Total Employee Wage for company " + company + " is: " + total_Emp_Wage + "\n");
+        }
+        public override string ToString()
+        {
+            return "Total Employee Wage for company: " + company + " is: " + total_Emp_Wage + "\n";
+        }
+    }
+}
