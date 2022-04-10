c8958cb01bbaeff2d3ecd0e5c0b9c289f6d5660e
satyamwathrey <swathrey1@gmail.com>
2022-04-10 22:43:28 +0530

    Ability to manage Employee Wage for Multiple Companies

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    class CompanyEmpWage
+    {
+        public string company;
+        public int emp_Wage_Per_Hr;
+        public int working_Days_Per_Month;
+        public int max_Hrs_Per_Month;
+        public int total_Emp_Wage;
+
+        public CompanyEmpWage(string company, int emp_Wage_Per_Hr, int working_Days_Per_Month, int max_Hrs_Per_Month)
+        {
+            this.company = company;
+            this.emp_Wage_Per_Hr = emp_Wage_Per_Hr;
+            this.working_Days_Per_Month = working_Days_Per_Month;
+            this.max_Hrs_Per_Month = max_Hrs_Per_Month;
+        }
+
+        public void SetTotalEmpWage(int totalEmpWage)
+        {
+            this.total_Emp_Wage = totalEmpWage;
+        }
+        public override string ToString()
+        {
+            return "Total Employee Wage for company: " + company + " is: " + total_Emp_Wage + "\n";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,30 +6,52 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
+             //Uc-1 call method
             //EmpAttendance.GetEmpAttendance();
+
+            //Uc-2 call method
             //EmployeeWage.GetEmpWage();
+
+            //Uc-3 call method
             //PartTimeEmployee.PartTimeWage();
+
+            //UC-4 call method
             //SwitchCase.DailyEmpWage();
+
+            //UC-5 call method
             //MonthWages.MonthlyEmpWage();
+
+            //UC-6 call method
             //WageTillCondition.GetWageTillCondition();
+
+            //UC-7 call method
             //RefactorWithClassMethod.RefactorGetMonthlyEmpWage();
+
+            //UC-8 call methods
             //EmpWageForMultipleCompanies.ComputeEmpWage("Google", 20, 2, 10);
             //EmpWageForMultipleCompanies.ComputeEmpWage("Microsoft", 10, 4, 20);
             //EmpWageForMultipleCompanies.ComputeEmpWage("Deloitte", 15, 3, 15);
 
+            //UC-9 call methods
+            //SaveTotalWageByCompany google = new SaveTotalWageByCompany("Google", 10, 4, 20);
+            //google.TotalWageForEachCompany();
+            //Console.WriteLine(google.ToString());
 
-            SaveTotalWageByCompany google = new SaveTotalWageByCompany("Google", 10, 4, 20);
-            google.TotalWageForEachCompany();
-            Console.WriteLine(google.ToString());
+            //SaveTotalWageByCompany microsoft = new SaveTotalWageByCompany("Microsoft", 20, 5, 20);
+            //microsoft.TotalWageForEachCompany();
+            //Console.WriteLine(microsoft.ToString());
 
-            SaveTotalWageByCompany microsoft = new SaveTotalWageByCompany("Microsoft", 20, 5, 20);
-            microsoft.TotalWageForEachCompany();
-            Console.WriteLine(microsoft.ToString());
+            //SaveTotalWageByCompany deloitte = new SaveTotalWageByCompany("Deloitte", 30, 3, 30);
+            //deloitte.TotalWageForEachCompany();
+            //Console.WriteLine(deloitte.ToString());
 
-            SaveTotalWageByCompany deloitte = new SaveTotalWageByCompany("Deloitte", 30, 3, 30);
-            deloitte.TotalWageForEachCompany();
-            Console.WriteLine(deloitte.ToString());
+            //UC-10 call methods
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.AddCompanyEmpWage("Google", 10, 4, 20);
+            empWageBuilder.AddCompanyEmpWage("Microsoft", 20, 5, 20);
+            empWageBuilder.AddCompanyEmpWage("Deloitte", 30, 3, 30);
+            empWageBuilder.ComputeEmpWage();
+
             
             Console.ReadLine();
         }
